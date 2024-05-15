0412f2f4f026d47a4ca24762692c53b469ecd7b8
Valeria Jarquin <vjarquin99@gmail.com>
2024-05-15 15:35:33 -0600

    Funcion que encuentra valor minimo del arreglo

--- ej2.c
+++ ej2.c
@@ -2,6 +2,19 @@
 
 #include<stdio.h>
 
+int encontrarMin(int arreglo[], int longitud){
+    int min = arreglo[0];
+
+    // Se recorre el arreglo
+    for(int i = 0; i < longitud; i++){
+        // Compracion del elemento actual con el min, si es menor que min lo actualiza al elemento menor
+        if(arreglo[i] < min){
+            min = arreglo[i];
+        }
+    }
+return min;
+}
+
 int encontrarMax(int arreglo[], int longitud){
     int max = arreglo[0];
 
@@ -20,6 +33,9 @@ int main(){
     int arreglo[] = {21,24,65,3,56,12,35,15};
     // Se calcula la cantidad de elementos en el arreglo
     int longitud = sizeof(arreglo) / sizeof(arreglo[0]);
+
+    int min = encontrarMin(arreglo, longitud);
+    printf("El valor mínimo es %d\n", min);
 
     int max = encontrarMax(arreglo, longitud);
     printf("El valor máximo es %d\n", max);
