46bc50644f763484a406429cbe0290a448f849dc
Valeria Jarquin <vjarquin99@gmail.com>
2024-05-14 22:25:26 -0600

    Segunda funcion ejercicio 1

--- ej1.c
+++ ej1.c
@@ -7,13 +7,39 @@ int searchLineal(int num, int arreglo[], int longitud){
     for(int i = 0; i < longitud; i++){
         // Se verifica si el numero ingresado se encuentra en el arreglo
         if (arreglo[i] == num){
-            printf("El numero %d se encuentra en la posicion %d del arreglo.\n", num, i);
+            printf("Busqueda lineal: el numero %d se encuentra en la posicion %d del arreglo.\n", num, i);
             return i;
         }
     };
-    printf("El numero %d no se encuentra en el arreglo.\n", num);
+    printf("Busqueda lineal: el numero %d no se encuentra en el arreglo.\n", num);
     return 1;
     
+}
+
+// Funcion de busqueda binaria dentro del arreglo
+int searchBinaria(int num, int arreglo[], int longitud){
+    int a = 0;
+    int b = longitud - 1;
+    int c;
+
+    // Se recorre el arreglo
+    while(a <= b){
+        c = a + (b-a)/2;
+        // Si el numero se encuentra en c, devuelve verdadero
+        if(arreglo[c] == num){
+            printf("Busqueda binaria: el numero %d se encuentra en la posicion %d del arreglo.\n", num, c);
+            return c;
+        // Si el numero es menor que c, busca en la mitad derecha del arreglo
+        } else if (arreglo[c] < num){
+            a = c + 1;
+        // De otra manera, busca en la mitad izquierda del arreglo
+        } else{
+            b = c - 1;
+        }
+    }
+
+    printf("Busqueda binaria: el numero %d no se encuentra en el arreglo.\n", num);
+    return 1;
 }
 
 
@@ -27,7 +53,8 @@ int main(){
     scanf("%d", &num);
 
     searchLineal(num, arreglo, longitud);
-
+    searchBinaria(num, arreglo, longitud);
+    
     return 0;
 
 }
