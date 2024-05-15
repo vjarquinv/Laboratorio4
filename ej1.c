525507f5d1068042173e1fd68f2bbe9846935f87
Valeria Jarquin <vjarquin99@gmail.com>
2024-05-14 23:12:26 -0600

    Tercera funcion ejercicio1

--- ej1.c
+++ ej1.c
@@ -16,7 +16,7 @@ int searchLineal(int num, int arreglo[], int longitud){
     
 }
 
-// Funcion de busqueda binaria dentro del arreglo
+// Funcion de busqueda binaria dentro del arreglo (a = inicio arreglo, b = fin del arreglo, c = medio del arreglo)
 int searchBinaria(int num, int arreglo[], int longitud){
     int a = 0;
     int b = longitud - 1;
@@ -37,12 +37,32 @@ int searchBinaria(int num, int arreglo[], int longitud){
             b = c - 1;
         }
     }
-
     printf("Busqueda binaria: el numero %d no se encuentra en el arreglo.\n", num);
     return 1;
 }
 
+// Funcion de busqueda binaria recursiva dentro del arreglo (d = inicio arreglo, e = fin del arreglo, f = medio del arreglo)
+int searchBinariaRecursiva(int num, int arreglo[], int d, int e){ 
+    // Caso donde el numero no se esta en el arreglo
+    if(d > e){
+        printf("Busqueda binaria recursiva: el numero %d no se encuentra en el arreglo.\n", num);
+        return 1;
+    }
 
+    int f = d + (e - d)/2;
+    // Si el numero se encuentra en d, devuelve verdadero
+    if(arreglo[f] == num){
+        printf("Busqueda binaria recursiva: el numero %d se encuentra en la posicion %d del arreglo.\n", num, f);
+        return f;
+    // Si el numero es menor que f, busca en la mitad derecha del arreglo y devuelve la funcion
+    } else if(arreglo[f] < num){
+        return searchBinariaRecursiva(num, arreglo, f + 1, e);
+    } else{
+    // De otra manera, busca en la mitad izquierda del arreglo y devuelve la funcion
+        return searchBinariaRecursiva(num, arreglo, d, f - 1);
+    }
+
+}
 
 int main(){
     int arreglo[] = {2,4,6,23,56,79};
@@ -54,6 +74,7 @@ int main(){
 
     searchLineal(num, arreglo, longitud);
     searchBinaria(num, arreglo, longitud);
+    searchBinariaRecursiva(num, arreglo, 0, longitud - 1);
     
     return 0;
 
