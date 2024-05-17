9b85cbcffaecb8615580de9e43793b9bd500be57
Valeria Jarquin <vjarquin99@gmail.com>
2024-05-17 16:50:26 -0600

    Funcion que encuentra valor minimo y maximo con punteros

--- ej2.c
+++ ej2.c
@@ -28,6 +28,23 @@ int encontrarMax(int arreglo[], int longitud){
 return max;
 }
 
+void encontrarMinMax(int *arreglo, int * minimo, int * maximo, int longitud){
+    *minimo = arreglo[0];
+    *maximo = arreglo[0];
+
+    // Se recorre el arreglo
+    for(int i = 0; i < longitud; i++){
+        // Compracion del elemento actual con el min, si es menor que min lo actualiza al elemento menor
+        if(arreglo[i] < *minimo){
+            *minimo = arreglo[i];
+        }
+
+        // Compracion del elemento actual con el max, si es mayor que max lo actualiza al elemento mayor
+        if(arreglo[i] > *maximo){
+            *maximo = arreglo[i];
+        }
+    }
+}
 
 int main(){
     int arreglo[] = {21,24,65,3,56,12,35,15};
@@ -39,6 +56,11 @@ int main(){
 
     int max = encontrarMax(arreglo, longitud);
     printf("El valor máximo es %d\n", max);
+
+    int minimo, maximo;
+    encontrarMinMax(arreglo, &minimo, &maximo, longitud);
+    printf("Punteros: el valor mínimo es %d\n", minimo);
+    printf("Punteros: el valor máximo es %d\n", maximo);
     
 
     return 0;
